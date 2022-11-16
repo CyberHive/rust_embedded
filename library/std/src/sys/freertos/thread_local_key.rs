db94d2ed40677a2c6b0943a2db49a2edf6c024d3
David Horton <david.horton@100percentit.com>
2022-11-16 15:09:57 +0000

    Add thread local key support for freertos

--- library/std/src/sys/freertos/thread_local_key.rs
+++ library/std/src/sys/freertos/thread_local_key.rs
@@ -1,26 +1,47 @@
+use crate::sync::atomic::{AtomicUsize, Ordering};
+use freertos_rust::*;
+
+//Not included in freertos rust bindings
+extern "C" {
+    fn vTaskSetThreadLocalStoragePointer(xTaskToSet: FreeRtosTaskHandle,  xIndex: usize, pvValue: *mut u8);
+    fn pvTaskGetThreadLocalStoragePointer(xTaskToQuery: FreeRtosTaskHandle, xIndex: usize) -> *mut u8;
+}
+
 pub type Key = usize;
+
+//TODO: Implement a key pool allowing keys to be added and removed
+//For now, just use a counting index.
+static mut key_index: AtomicUsize = AtomicUsize::new(0);
+const MAX_KEYS: usize = 5; //This must correspond to configNUM_THREAD_LOCAL_STORAGE_POINTERS, but not yet enforced
+
 
 #[inline]
 pub unsafe fn create(_dtor: Option<unsafe extern "C" fn(*mut u8)>) -> Key {
-    panic!("should not be used on this target");
+    let ret = key_index.fetch_add(1, Ordering::Relaxed);
+    assert!(ret < MAX_KEYS);
+    ret
 }
 
+// Set the value associated with this TLS key
 #[inline]
-pub unsafe fn set(_key: Key, _value: *mut u8) {
-    panic!("should not be used on this target");
+pub unsafe fn set(key: Key, value: *mut u8) {
+    assert!(key < MAX_KEYS);
+    vTaskSetThreadLocalStoragePointer(0 as FreeRtosTaskHandle, key, value );
 }
 
+// Get the value associated with this TLS key
 #[inline]
-pub unsafe fn get(_key: Key) -> *mut u8 {
-    panic!("should not be used on this target");
+pub unsafe fn get(key: Key) -> *mut u8 {
+    assert!(key < MAX_KEYS);
+    pvTaskGetThreadLocalStoragePointer(0 as FreeRtosTaskHandle, key )
 }
 
 #[inline]
 pub unsafe fn destroy(_key: Key) {
-    panic!("should not be used on this target");
+    //Nothing here yet!
 }
 
 #[inline]
 pub fn requires_synchronized_create() -> bool {
-    panic!("should not be used on this target");
+    false
 }
