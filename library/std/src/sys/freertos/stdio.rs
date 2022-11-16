64aaf16af7df656670e016db288562f2ebee57f1
David Horton <david.horton@100percentit.com>
2022-11-16 14:14:27 +0000

    Implemented stdout, stderror and panic_output

--- library/std/src/sys/freertos/stdio.rs
+++ library/std/src/sys/freertos/stdio.rs
@@ -4,6 +4,11 @@ pub struct Stdin;
 pub struct Stdout;
 pub struct Stderr;
 
+//We temporarily reference a C support function to access the UART driver. To be replaced with a Rust version shortly
+extern "C" {
+    fn console_print_n(fmt: *const i8, len: i32);
+}
+
 impl Stdin {
     pub const fn new() -> Stdin {
         Stdin
@@ -24,6 +29,9 @@ impl Stdout {
 
 impl io::Write for Stdout {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        unsafe {
+            console_print_n(buf.as_ptr() as *const i8, buf.len() as i32);
+        }
         Ok(buf.len())
     }
 
@@ -40,6 +48,9 @@ impl Stderr {
 
 impl io::Write for Stderr {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        unsafe {
+            console_print_n(buf.as_ptr() as *const i8, buf.len() as i32);
+        }
         Ok(buf.len())
     }
 
@@ -54,6 +65,6 @@ pub fn is_ebadf(_err: &io::Error) -> bool {
     true
 }
 
-pub fn panic_output() -> Option<Vec<u8>> {
-    None
+pub fn panic_output() -> Option<impl io::Write> {
+    Some(Stderr::new())
 }
